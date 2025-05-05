4c5ec770757b42c224411a7413dc494cb6756504
aising <tasignas@gmail.com>
2025-05-05 20:11:42 +0300

    add method mapping to constants

--- app/services/evm/constants.rb
+++ app/services/evm/constants.rb
@@ -1,8 +1,8 @@
 class Evm::Constants
-  METHODS = {
-    get_balance: 'eth_getBalance',
-    get_transaction_count: 'eth_getTransactionCount',
-    get_block_number: 'eth_blockNumber',
-    get_block_by_number: 'eth_getBlockByNumber'
-  }.freeze
+  METHOD_MAP = {
+   'eth_getBalance' => { method: :balance, required_params: [:address] },
+   'eth_getTransactionCount' => { method: :tx_count, required_params: [:address] },
+   'eth_blockNumber' => { method: :block_number, required_params: [] },
+   'eth_getBlockByNumber' => { method: :block_by_number, required_params: [:block_number] }
+ }.freeze
 end
