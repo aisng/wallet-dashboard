ab09922d883073a149b5736890e756a7e05e4b3a
aising <tasignas@gmail.com>
2025-05-15 00:44:19 +0300

    fix: dashboard controller params
    - testnet not required
    - block_tag allow blank
    - block_number of type :number

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -13,11 +13,11 @@ class Api::V1::DashboardController < ApplicationController
   api :POST, '/api/v1/dashboard', 'Call a blockchain RPC method'
   param :dashboard, Hash, required: true do
     param :chain, String, required: true, desc: 'Blockchain name (e.g. Ethereum)'
-    param :testnet, :bool, required: true, desc: 'Flag to indicate if the testnet is used', allow_blank: true
+    param :testnet, :bool, required: false, desc: 'Flag to indicate if the testnet is used', allow_blank: true
     param :method, String, required: true, desc: 'RPC method to call (e.g. eth_getBalance)'
     param :address, String, required: false, desc: 'Wallet address'
-    param :block_tag, String, required: false, desc: 'Block tag (e.g. latest, pending) or block number in decimal'
-    param :block_number, String, required: false, desc: 'Block number in decimal (e.g. 12345)', allow_blank: true
+    param :block_tag, String, required: false, desc: 'Block tag (e.g. latest, pending) or block number in decimal', allow_blank: true
+    param :block_number, :number, required: false, desc: 'Block number in decimal (e.g. 12345)', allow_blank: true
     param :full_transaction, :bool, required: false, desc: 'Flag to indicate if full transaction details are required', allow_blank: true
   end
   def rpc_action
